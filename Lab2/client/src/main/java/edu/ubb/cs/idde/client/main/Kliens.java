bc48e0351e4896e9ecc86282cdc05db0039e5a0e
timeakispal <timeakispal@yahoo.com>
2016-04-28 12:05:06 +0300

    Modified client to display data in Jtable

--- Lab2/client/src/main/java/edu/ubb/cs/idde/client/main/Kliens.java
+++ Lab2/client/src/main/java/edu/ubb/cs/idde/client/main/Kliens.java
@@ -3,7 +3,6 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
-import javax.swing.table.AbstractTableModel;
 
 import edu.ubb.cs.idde.server.main.GenericDao;
 import edu.ubb.cs.idde.server.main.GenericJdbcDao;
@@ -14,11 +13,11 @@ import java.beans.PropertyDescriptor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.Vector;
 
 public class Kliens extends JFrame implements ActionListener{
     /**
@@ -36,7 +35,6 @@ public class Kliens extends JFrame implements ActionListener{
         button.addActionListener(this);
         add(panel,BorderLayout.CENTER);
         add(button, BorderLayout.NORTH);
-
     }
     
     @Override
@@ -50,21 +48,24 @@ public class Kliens extends JFrame implements ActionListener{
         List<States> l2 = dao2.getAllDataRows();
     	Iterator<States> i2 = l2.listIterator();
     	
-    	ArrayList<String> columnNames = new ArrayList<String>();
+    	Vector<String> columnNames = new Vector<String>();
     	for (Field field : l2.get(0).getClass().getDeclaredFields()) {
 			columnNames.add(messages.getString(field.getName()));
 		}
     	
-    	while( i2.hasNext())
+    	Vector<Vector<Object>> data = new Vector<Vector<Object>>();
+
+		while( i2.hasNext())
     	{
     		States c = i2.next();
+    		Vector<Object> row = new Vector<Object>();
     		for (Field field : c.getClass().getDeclaredFields()) {
 				PropertyDescriptor propertyDescriptor;
 				try {
 					propertyDescriptor = new PropertyDescriptor(field.getName(), c.getClass());
     				Method method = propertyDescriptor.getReadMethod();
     				
-    				// ezt hozzaadom a tablehez amit meg akarok jeleniteni
+    				row.add(method.invoke(c));
     				System.out.println(messages.getString(field.getName())+" = "+ method.invoke(c));
 
 				} catch (IntrospectionException e) {
@@ -80,13 +81,11 @@ public class Kliens extends JFrame implements ActionListener{
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-
 			}
+    		data.add(row);
     	}
         
-    	CustomModel<States>  model = new CustomModel<States>(l2);
-        JTable table = new JTable();
-        table.setModel(model);
+        JTable table = new JTable(data,columnNames);
         panel.add(new JScrollPane(table));
         repaint();
         revalidate();
@@ -99,49 +98,4 @@ public class Kliens extends JFrame implements ActionListener{
         s.pack();
         s.setVisible(true);
     }
-}
-
-class CustomModel<T> extends AbstractTableModel {
-
-    /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-    String[] columnNames = {"ID", "State Name", "Population"};
-    List<T> data;
-    
-    public CustomModel(List<T> list) {
-        data = list;
-    }
-
-    @Override
-    public String getColumnName(int column) {
-        return columnNames[column];
-    }
-
-    @Override
-    public int getColumnCount() {
-        return 3;
-    }
-
-    @Override
-    public int getRowCount() {
-        return data.size();
-    }
-
-    @Override
-    public Object getValueAt(int rowIndex, int columnIndex) {
-        T stateInfo = data.get(rowIndex);
-        switch (columnIndex) {
-        case 0:
-            return ((States) stateInfo).getId();
-        case 1:
-            return ((States) stateInfo).getState();
-        case 2:
-            return ((States) stateInfo).getPopulation();
-        default:
-            return null;
-        }
-    }
-
 }
